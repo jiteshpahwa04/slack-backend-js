9e4dd21157f42b7f1fcec0e82ade29200cfba2ce
Jitesh Pahwa <jiteshofficial2004@gmail.com>
2025-11-23 11:36:47 +0530

    Added message and related schemas

---
+++ src/schema/workspace.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const workspaceSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Workspace name is required']
+    },
+    description: {
+        type: String,
+    },
+    members: [{
+        memberId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        },
+        role: {
+            type: String,
+            enum: ['admin', 'member'],
+            default: 'member'
+        }
+    }],
+    joinCode: {
+        type: String,
+        required: [true, 'Join code is required']
+    },
+    channels: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Channel'
+    }]
+}, {
+    timestamps: true
+});
+
+const Workspace = mongoose.model('Workspace', workspaceSchema);
+
+export default Workspace;
